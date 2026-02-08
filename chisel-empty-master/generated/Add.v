f5506568650aaf7e1a21c841c036423f28aaa128
G0bica <matic.sega11@gmail.com>
2026-02-08 04:45:06 +0100

    added src code

--- chisel-empty-master/generated/Add.v
+++
@@ -1,8 +0,0 @@
-module Add(
-  input   clock,
-  input   reset,
-  output  io_led, // @[\\src\\main\\scala\\empty\\Add.scala 14:14]
-  input   io_sw // @[\\src\\main\\scala\\empty\\Add.scala 14:14]
-);
-  assign io_led = io_sw; // @[\\src\\main\\scala\\empty\\Add.scala 19:10]
-endmodule
